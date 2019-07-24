7165a111f31b6dcfcd0d4531c42d5153ac9448ec
George Kafentzis <50694072+gpk1983@users.noreply.github.com>
2019-07-24 12:06:36 +0300

    Create file5.m

---
+++ Chapter13/file5.m
@@ -0,0 +1,18 @@
+No = 4;                                      % Παράμετρος σήματος
+N = 81;                                      % Διάρκεια σήματος
+NFFT = 512;                                  % N σημεία του DFT
+x1 = 3 * cos(3*pi*(-40:40)/No););            % Σήμα x1[n]
+fx1 = fft(x1, NFFT);                         % DFT
+mag = abs(fx1);                              % Φάσμα πλάτους
+ph = angle(fx1);                             % Φάσμα φάσης
+omega = 2*pi*(0:NFFT-1)./NFFT;               % Άξονας ω
+
+figure; subplot(311); stem(-40:40, x1); grid;        % Γράφημα x1[n]
+xlabel('Time (samples)'); ylabel('Amplitude');     % Ετικέτες
+title('Signal in time');                           % Τίτλος
+subplot(312); plot(omega, mag); grid;              % Γράφημα πλάτους
+ylabel('Magnitude'); xlabel('Frequency (\omega)'); % Ετικέτες
+title('Magnitude Spectrum');                       % Τίτλος
+subplot(313); plot(omega, ph); grid;               % Γράφημα φάσης
+ylabel('Phase'); xlabel('Frequency (\omega)');     % Ετικέτες
+title('Phase Spectrum');                           % Τίτλος
