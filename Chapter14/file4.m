9fafc11079b1e605bf6df2b49a1dd79c4f00e9c9
George Kafentzis <50694072+gpk1983@users.noreply.github.com>
2019-07-24 12:19:03 +0300

    Create file4.m

---
+++ Chapter14/file4.m
@@ -0,0 +1,12 @@
+[x, fs] = audioread('myvoice.wav');             % Σήμα φωνής
+num = [0.0181, 0.0543, 0.0543, 0.0181];         % Αριθμητής
+den = [1, -1.76, 1.1829, -0.2781];              % Παρονομαστής
+y = filter(num, den, x);                        % Φιλτράρισμα
+
+t = 0:1/fs:length(x)/fs-1/fs;                   % Συνεχης άξονας χρόνου
+subplot(211); plot(t, x); grid;                 % Γράφημα σήματος φωνής
+xlabel('Time (s)'); ylabel('Amplitude');        % Ετικέτες
+title('Input speech signal');                   % Τίτλος
+subplot(212); plot(t, y); grid;                 % Έξοδος φίλτρου
+xlabel('Time (s)'); ylabel('Amplitude');        % Ετικέτες
+title('Output speech signal');                  % Τίτλος
